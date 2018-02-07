1fc4bbbadc90d527b9e7b232acc5d33e61f24844
Ariel <galiano.ariel@gmail.com>
2018-02-07 00:25:53 -0300

    Added tilling background.

---
+++ Assets/BackgroundComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BackgroundComponent : MonoBehaviour
+{
+
+	[SerializeField] private Camera _camera;
+	[SerializeField] private float _speed = 1f;
+	private Renderer _rendererComponent;
+
+	private void Start()
+	{
+		_rendererComponent = GetComponent<Renderer>();
+	}
+
+	void Update ()
+	{
+		var xOffset = _camera.transform.position.x;
+		var offset = new Vector2(xOffset * _speed , 0);
+		_rendererComponent.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
