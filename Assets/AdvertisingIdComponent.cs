ca247bfb551d1f0c4aef2d3628761e50305b6d5e
Emmanuel <pickappgames@gmail.com>
2018-02-10 10:51:43 -0300

    Mono to obtain advertising id.

---
+++ Assets/AdvertisingIdComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AdvertisingIdComponent : MonoBehaviour{
+
+    private void Start()
+    {
+        Application.RequestAdvertisingIdentifierAsync (
+            (string advertisingId, bool trackingEnabled, string error) =>
+            { Debug.Log ("advertisingId " + advertisingId + " " + trackingEnabled + " " + error); }
+        );
+    }
+}
