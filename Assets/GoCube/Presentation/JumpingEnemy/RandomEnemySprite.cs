6a815eb8fb703ce8688f73e194ac8d8c691e81d6
Ariel <galiano.ariel@gmail.com>
2018-04-01 09:39:34 -0300

    Added random sprite selector for enemies. Fixed colliders.

---
+++ Assets/GoCube/Presentation/JumpingEnemy/RandomEnemySprite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GoCube.Presentation.JumpingEnemy
+{
+	public class RandomEnemySprite : MonoBehaviour {
+		private void Start()
+		{
+			GetComponent<Animator>().SetInteger("EnemyType", Random.Range(1,3));
+		}
+	}
+}
