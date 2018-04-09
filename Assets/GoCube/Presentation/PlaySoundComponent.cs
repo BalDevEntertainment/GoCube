41b278c6aa3a105983fa1a45182ec4596ca7f035
Ariel <galiano.ariel@gmail.com>
2018-04-09 01:10:46 -0300

    Added screenshots. Removed some unnecesary sounds.

---
+++ Assets/GoCube/Presentation/PlaySoundComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GoCube.Presentation
+{
+	public class PlaySoundComponent : MonoBehaviour
+	{
+		private AudioSource _audioSource;
+
+		private void Start()
+		{
+			_audioSource = GetComponent<AudioSource>();
+		}
+
+		public void PlaySound()
+		{
+			_audioSource.Play();
+		}
+	}
+}
